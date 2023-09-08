8c0673a0331aee5a0645dd370e6ffc44b54103f5
Deadsg <144394753+Deadsg@users.noreply.github.com>
2023-09-07 23:46:24 -0400

    Add files via upload

---
+++ Batsy4.py
@@ -0,0 +1,320 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+import torch
+import tensorflow as tf
+import onnx
+
+from transformers import AutoTokenizer
+from safe_rlhf.models import AutoModelForScore
+
+import discord
+from discord.ext import commands
+
+import gym
+
+# Load the Transformers model and tokenizer
+model = AutoModelForScore.from_pretrained('PKU-Alignment/beaver-7b-v1.0-reward', device_map='auto')
+tokenizer = AutoTokenizer.from_pretrained('PKU-Alignment/beaver-7b-v1.0-reward', use_fast=False)
+
+input_text = 'BEGINNING OF CONVERSATION: USER: hello ASSISTANT: Hello! How can I help you today?'
+
+input_ids = tokenizer(input_text, return_tensors='pt')
+output = model(**input_ids)
+print(output)
+
+# Load PyTorch and TensorFlow models
+pytorch_model = torch.load('your_pytorch_model.pth')
+tf_model = tf.keras.models.load_model('your_tensorflow_model.h5')
+
+# Convert to ONNX
+onnx_model = onnx.export(pytorch_model, ...)
+
+# Define the Discord bot
+intents = discord.Intents.default()
+intents.typing = True
+intents.presences = True
+client = commands.Bot(command_prefix="!", intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        await message.channel.send('Hello!')
+
+# Replace 'YOUR_BOT_TOKEN' with your Discord bot token
+client.run('YOUR_BOT_TOKEN')
+
+# Define the environment
+grid = np.array([[0, 0, 1, 0, 0],
+                 [0, 0, 1, 0, 1],
+                 [0, 0, 0, 0, 1],
+                 [0, 1, 1, 0, 0],
+                 [0, 0, 0, 0, 2]])
+
+# Initialize Q-table
+num_states = grid.size
+num_actions = 4
+q_table = np.zeros((num_states, num_actions))
+
+# Q-learning parameters
+learning_rate = 0.8
+discount_factor = 0.95
+exploration_prob = 0.2
+episodes = 1000
+
+# Helper function to get next state and reward
+def get_next_state(current_state, action):
+    # Implement your logic here
+    pass
+
+# Q-learning algorithm
+for _ in range(episodes):
+    current_state = (0, 0)
+
+    while True:
+        if np.random.uniform(0, 1) < exploration_prob:
+            action = np.random.randint(num_actions)
+        else:
+            action = np.argmax(q_table[current_state[0] * grid.shape[1] + current_state[1]])
+
+        next_state, reward = get_next_state(current_state, action)
+
+        # Update Q-table
+        # Implement your Q-table update logic here
+
+        if reward == 2:  # Reached the goal
+            break
+
+        current_state = next_state
+
+# Define a simple model with self-updating weights
+class SelfUpdatingModel:
+    def __init__(self):
+        self.weights = [0.5, 0.3, -0.2]
+
+    def predict(self, features):
+        return sum(w * f for w, f in zip(self.weights, features))
+
+    def update_weights(self, features, target, learning_rate):
+        prediction = self.predict(features)
+        error = target - prediction
+        self.weights = [w + learning_rate * error * f for w, f in zip(self.weights, features)]
+
+# Example usage of the SelfUpdatingModel
+model = SelfUpdatingModel()
+data = [([1, 2, 3], 7), ([2, 3, 4], 12), ([3, 4, 5], 17)]
+
+for features, target in data:
+    prediction = model.predict(features)
+    print(f"Predicted: {prediction}, Actual: {target}")
+
+    model.update_weights(features, target, learning_rate=0.1)
+    print(f"Updated weights: {model.weights}")
+
+# Generate sample data for linear regression
+np.random.seed(0)
+X = 2 * np.random.rand(100, 1)
+y = 4 + 3 * X + np.random.randn(100, 1)
+
+# Train-test split
+X_train, X_test = X[:80], X[80:]
+y_train, y_test = y[:80], y[80:]
+
+# Train the linear regression model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Predict on the test set
+y_pred = model.predict(X_test)
+
+# Evaluate the model
+mse = mean_squared_error(y_test, y_pred)
+print(f"Mean Squared Error: {mse}")
+
+# Visualize the results
+plt.scatter(X_test, y_test, color='blue')
+plt.plot(X_test, y_pred, color='red', linewidth=2)
+plt.show()
+
+def objective_function(x):
+    return -(x ** 2)  # Negative because we want to find the maximum
+
+def hill_climbing(starting_point, step_size, num_iterations):
+    current_point = starting_point
+
+    for _ in range(num_iterations):
+        current_value = objective_function(current_point)
+
+        # Evaluate neighboring points
+        left_neighbor = current_point - step_size
+        right_neighbor = current_point + step_size
+
+        left_value = objective_function(left_neighbor)
+        right_value = objective_function(right_neighbor)
+
+        # Move to the neighbor with the higher value
+        if left_value > current_value:
+            current_point = left_neighbor
+        elif right_value > current_value:
+            current_point = right_neighbor
+
+    return current_point, objective_function(current_point)
+
+if __name__ == "__main__":
+    starting_point = 2
+    step_size = 0.1
+    num_iterations = 100
+
+    final_point, max_value = hill_climbing(starting_point, step_size, num_iterations)
+
+    print(f"The maximum value is {max_value} at x = {final_point}")
+
+    # Create a bot instance with a command prefix
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+
+    # Initialize the Discord bot
+intents = discord.Intents.default()
+intents.typing = max_features
+intents.presences = max_features
+client = discord.Client(intents=intents)
+
+# Set your OpenAI API key here
+openai.api_key = "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"
+
+# Define the bot's prefix and command
+BOT_PREFIX = "!"
+COMMAND = "ask"
+
+@client.event
+async def on_ready():
+    print(f"We have logged in as {client.user}")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith(f"{BOT_PREFIX}{COMMAND} "):
+        # Extract the user's question
+        user_question = message.content[len(BOT_PREFIX + COMMAND) + 1 :]
+
+        # Generate a response using OpenAI GPT-3
+        response = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt=f"Ask a question: {user_question}\nAnswer:",
+            max_tokens=50,  # Adjust this as needed
+        )
+
+        # Send the response back to the user
+        await message.channel.send(response.choices[0].text)
+
+        # Run the bot
+client.run(os.getenv(MTE0NjkwNDk2Nzc2NTA1MzQ2MA.GoiSsO.lx34OI2aYPIXNw8fKz0TBoyisGAMxvIGgx1dKU))  # Use your bot token here
+
+python discord_bot.py
+
+Note: Be mindful of OpenAI API usage and billing limits. Depending on your usage, you may need to consider API rate limiting and costs.
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Who won the world series in 2020?"},
+        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        {"role": "user", "content": "Where was it played?"}
+    ]
+)
+openai.FineTuningJob.create(training_file="file-abc123", model="gpt-3.5-turbo")
+# List 10 fine-tuning jobs
+openai.FineTuningJob.list(limit=10)
+
+# Retrieve the state of a fine-tune
+openai.FineTuningJob.retrieve("ft-abc123")
+
+# Cancel a job
+openai.FineTuningJob.cancel("ft-abc123")
+
+# List up to 10 events from a fine-tuning job
+openai.FineTuningJob.list_events(id="ft-abc123", limit=10)
+
+# Delete a fine-tuned model (must be an owner of the org the model was created in)
+openai.Model.delete("ft-abc123")
+completion = openai.ChatCompletion.create(
+  model="ft:gpt-3.5-turbo:my-org:custom_suffix:id",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Hello!"}
+  ]
+)
+print(completion.choices[0].message)
+{
+    "object": "fine_tuning.job.event",
+    "id": "ftevent-abc-123",
+    "created_at": 1693582679,
+    "level": "info",
+    "message": "Step 100/100: training loss=0.00",
+    "data": {
+        "step": 100,
+        "train_loss": 1.805623287509661e-5,
+        "train_mean_token_accuracy": 1.0
+    },
+    "type": "metrics"
+}
+
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, classification_report
+
+# Sample data
+data = {
+    'text': ['I love this product', 'This is great', 'Awful product', 'Not good at all'],
+    'label': ['Positive', 'Positive', 'Negative', 'Negative']
+}
+
+# Create a DataFrame
+df = pd.DataFrame(data)
+
+# Split the data into training and testing sets
+X = df['text']
+y = df['label']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create a TF-IDF vectorizer
+tfidf_vectorizer = TfidfVectorizer(max_features=1000)
+
+# Fit and transform the training data
+X_train_tfidf = tfidf_vectorizer.fit_transform(X_train)
+
+# Transform the test data
+X_test_tfidf = tfidf_vectorizer.transform(X_test)
+
+# Create a Multinomial Naive Bayes classifier
+clf = MultinomialNB()
+
+# Train the classifier
+clf.fit(X_train_tfidf, y_train)
+
+# Make predictions on the test data
+y_pred = clf.predict(X_test_tfidf)
+
+# Evaluate the model
+accuracy = accuracy_score(y_test, y_pred)
+classification_rep = classification_report(y_test, y_pred)
+
+print(f"Accuracy: {accuracy}")
+print("\nClassification Report:\n", classification_rep)
+
