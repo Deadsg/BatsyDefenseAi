51cf2f1e345ca49a43d2aaf8d0160ce9fd950864
Deadsg <144394753+Deadsg@users.noreply.github.com>
2023-10-20 09:27:45 -0400

    Add files via upload

--- GreatestDetective.py
+++ GreatestDetective.py
@@ -1,371 +1,138 @@
+import discord
+from discord.ext import commands
+import joblib
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import make_pipeline
 import gym
-from gym import spaces
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.linear_model import LogisticRegression, SGDClassifier
-from onnxmltools import convert_sklearn
 import numpy as np
-import onnx
-import tf2onnx
-import onnxruntime as rt
-from onnxmltools import convert_sklearn
-from onnxruntime import InferenceSession
-from onnx import optimizer
+import openai
 
-# Convert your model to ONNX format
-onnx_model = onnx.convert(your_model, target_opset=11)  # Adjust target_opset based on your needs
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
 
-# Optimize the ONNX model (optional but recommended)
-optimized_model = optimizer.optimize(onnx_model)
+openai.api_key = ''
 
+bot = commands.Bot(command_prefix="!", intents=intents)
 
-class SelfLearningEnv(gym.Env):
-    def __init__(self):
-        super(SelfLearningEnv, self).__init__()
+env = gym.make('CartPole-v1')
 
-        # Define action and observation spaces
-        self.action_space = spaces.Discrete(2)  # Example: Two possible actions (0 and 1)
-        self.observation_space = spaces.Box(low=0, high=1, shape=(4,), dtype=np.float32)  # Example: Four-dimensional state space
+chat_data = []
 
-        # Define other parameters of the environment
-        self.max_steps = 100  # Maximum number of steps per episode
-        self.current_step = 0  # Current step
+acronym_dictionary = {
+    "AI": "Artificial Intelligence",
+    "ML": "Machine Learning",
+    "DL": "Deep Learning",
+    "NN": "Neural Network",
+    # Add more acronyms and their meanings as needed
+}
 
-        # Define any additional variables or parameters needed
+model = make_pipeline(CountVectorizer(), MultinomialNB())
+X = ["good", "bad", "awesome", "terrible"]
+y = [1, 0, 1, 0]  # 1 for positive, 0 for negative
+model.fit(X, y)
 
-    def reset(self):
-        # Reset the environment to the initial state
-        self.current_step = 0
+# Save the model to a file
+joblib.dump(model, 'sentiment_model.joblib')
 
-        # Return initial state as a numpy array
-        return np.array([0, 0, 0, 0])  # Example: Initial state with all zeros
+SPAM_THRESHOLD = 5
 
-    def step(self, action):
-        # Take an action and return the new state, reward, termination condition, and additional info
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
 
-        # Example: Updating the state based on the action
-        new_state = np.random.rand(4)  # Example: Generating a random new state
+@bot.command()
+async def openai_chat(ctx, *, message):
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": message}
+            ]
+        )
+        await ctx.send(response.choices[0].message['content'])
+    except Exception as e:
+        await ctx.send(f"An error occurred: {e}")
 
-        # Example: Calculating the reward based on the action and state
-        reward = np.sum(new_state) if action == 1 else -np.sum(new_state)
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello!")
 
-        # Update current step
-        self.current_step += 1
+@bot.command()
+async def reboot(ctx):
+    await ctx.send("Rebooting...")
+    await bot.close()
 
-        # Check if the episode is done (termination condition)
-        done = self.current_step >= self.max_steps
+@bot.command()
+async def record(ctx, *, message):
+    # Record the message and author's name
+    chat_data.append((ctx.author.name, message))
+    await ctx.send("Message recorded.")
 
-        # Return state, reward, done flag, and additional info (if any)
-        return new_state, reward, done, {}
+@bot.command()
+async def show_recorded_data(ctx):
+    if not chat_data:
+        await ctx.send("No recorded data.")
+    else:
+        formatted_data = "\n".join([f"{author}: {message}" for author, message in chat_data])
+        await ctx.send(formatted_data)
 
-    def render(self):
-        # Define how to visualize or display the environment (optional)
-        pass
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
 
-    def close(self):
-        # Clean up resources, if any (optional)
-        pass
+    content = message.content.lower()
+    if content in [msg.lower() for _, msg in chat_data]:
+        await message.channel.send(f"Potential spam detected. User {message.author.name} may be copying recorded messages.")
+    else:
+        await bot.process_commands(message)
+        formatted_data = "\n".join([f"{author}: {message}" for author, message in chat_data])
+        await ctx.send(formatted_data)
 
-# Register the environment with Gym
-gym.envs.register(
-    id='SelfLearning-v0',
-    entry_point='custom_envs.self_learning_env:SelfLearningEnv'
-)
+@bot.command()
+async def update_code(ctx):
+    # Assuming you have a new version of the code saved in a file named 'new_code.py'
+    with open('new_code.py', 'r') as file:
+        new_code = file.read()
 
-# Assuming you have a Gym environment named SelfLearningEnv
+    # Save the new code to 'bot.py' (current running file)
+    with open('bot.py', 'w') as file:
+        file.write(new_code)
 
-env = gym.make('SelfLearning-v0')
+    # Restart the bot
+    os.execl(sys.executable, sys.executable, *sys.argv)
 
-# Define a scikit-learn model (e.g., Linear Regression)
-model = LinearRegression()
-
-for episode in range(100):  # Train for 100 episodes
-    state = env.reset()
-
-    # Collect data for training
-    states = []
-    actions = []
+async def play_cartpole(ctx):
+    # Simulated anti-AI defense game using CartPole
+    obs = env.reset()
+    total_reward = 0
 
     while True:
-        # Generate a fake action for demonstration purposes
-        action = env.action_space.sample()
-
-        states.append(state)
-        actions.append(action)
-
-        next_state, _, done, _ = env.step(action)
-        state = next_state
+        action = 1 if np.random.random() < 0.5 else 0  # Random action as defense
+        obs, reward, done, _ = env.step(action)
+        total_reward += reward
 
         if done:
+            await ctx.send(f"Game over! Total reward: {total_reward}")
             break
 
-    # Train scikit-learn model on collected data
-    X = PolynomialFeatures(degree=2).fit_transform(states)
-    y = actions
-    model.fit(X, y)
-
-# Export the model to ONNX format
-initial_type = [('float_input', FloatTensorType([None, X.shape[1]]))]
-onnx_model = convert_sklearn(model, initial_types=initial_type)
-
-with open("self_learning_model.onnx", "wb") as f:
-    f.write(onnx_model.SerializeToString())
-
-# Generate some example data for a linear regression model
-np.random.seed(0)
-X_test = np.random.rand(10, 1) * 10
-
-# Assuming a linear relationship y = 3*X + noise
-y_test = 3 * X_test.squeeze() + np.random.randn(10)
-
-# Save the test data to a file
-np.savetxt('test_data.csv', np.concatenate((X_test, y_test.reshape(-1, 1)), axis=1), delimiter=',', header='Feature,Target', comments='')
-
-print("Test data generated and saved as 'test_data.csv'.")
-
-# Step 1: Train a scikit-learn model
-np.random.seed(0)
-X_train = np.random.rand(100, 1) * 10
-y_train = 3 * X_train.squeeze() + np.random.randn(100)
-
-model = LinearRegression()
-model.fit(X_train, y_train)
-
-# Step 2: Convert the model to ONNX format
-import skl2onnx
-from skl2onnx import convert_sklearn
-
-initial_type = [('float_input', skl2onnx.common.data_types.FloatTensorType([None, 1]))]
-onnx_model = convert_sklearn(model, initial_types=initial_type)
-
-# Save the ONNX model
-onnx.save_model(onnx_model, 'linear_regression.onnx')
-
-# Step 3: Create a Gym environment
-class LinearRegressionEnv(gym.Env):
-    def __init__(self):
-        super(LinearRegressionEnv, self).__init__()
-        self.action_space = gym.spaces.Discrete(1)
-        self.observation_space = gym.spaces.Box(low=0, high=10, shape=(1,), dtype=np.float32)
-
-    def reset(self):
-        self.state = np.random.rand(1) * 10
-        return self.state
-
-    def step(self, action):
-        return self.state, self.state[0]*3, True, {}
-
-# Step 4: Test the ONNX model in the Gym environment
-env = LinearRegressionEnv()
-
-session = rt.InferenceSession('linear_regression.onnx')
-input_name = session.get_inputs()[0].name
-output_name = session.get_outputs()[0].name
-
-for _ in range(10):
-    state = env.reset()
-    action = model.predict([state])[0]
-    onnx_input = {input_name: np.array([[state[0]]], dtype=np.float32)}
-    prediction = session.run([output_name], onnx_input)[0]
-    print(f"True Value: {action}, Predicted Value: {prediction[0][0]}")
-
-# Define a scikit-learn model
-class ScikitLearnModel:
-    def __init__(self):
-        self.model = LogisticRegression()
-
-    def train(self, X, y):
-        self.model.fit(X, y)
-
-    def predict(self, X):
-        return self.model.predict(X)
-
-# Define a simple neural network using TensorFlow
-class SimpleNeuralNetwork(tf.keras.Model):
-    def __init__(self):
-        super(SimpleNeuralNetwork, self).__init__()
-        self.dense = tf.keras.layers.Dense(1, activation='sigmoid')
-
-    def call(self, inputs):
-        return self.dense(inputs)
-
-# Convert TensorFlow model to ONNX format
-def convert_to_onnx(model):
-    dummy_input = tf.constant(np.zeros((1, 2), dtype=np.float32))  # Adjust input shape as needed
-    onnx_model = tf2onnx.convert.from_keras(model, dummy_input)
-    return onnx_model
-
-# Define a simple environment using OpenAI Gym
-class SimpleEnvironment(gym.Env):
-    def __init__(self):
-        super(SimpleEnvironment, self).__init__()
-        self.action_space = gym.spaces.Discrete(2)
-        self.observation_space = gym.spaces.Box(low=0, high=1, shape=(2,), dtype=np.float32)
-
-    def reset(self):
-        return np.random.rand(2)
-
-    def step(self, action):
-        return np.random.rand(2), 0, False, {}
-
-# Main command processing function
-def process_command(command):
-    global scikit_model, tf_model, onnx_model
-    if command.startswith("train_sklearn"):
-        # Parse and preprocess training data
-        X = np.random.rand(100, 2)
-        y = np.random.randint(2, size=100)
-        scikit_model.train(X, y)
-        print("Scikit-learn model trained.")
-    elif command.startswith("train_tf"):
-        # Define and train a TensorFlow model
-        X = np.random.rand(100, 2)
-        y = np.random.randint(2, size=100)
-        tf_model = SimpleNeuralNetwork()
-        tf_model.compile(optimizer='adam', loss='binary_crossentropy')
-        tf_model.fit(X, y, epochs=5, verbose=0)
-        print("TensorFlow model trained.")
-    elif command.startswith("convert_to_onnx"):
-        # Convert TensorFlow model to ONNX
-        onnx_model = convert_to_onnx(tf_model)
-        print("TensorFlow model converted to ONNX.")
-    elif command.startswith("predict_sklearn"):
-        # Generate a random input and make a prediction using scikit-learn model
-        input_data = np.random.rand(2).reshape(1, -1)
-        prediction = scikit_model.predict(input_data)
-        print(f"Scikit-learn model prediction: {prediction}")
-    elif command.startswith("predict_onnx"):
-        # Simulate inference using ONNX model
-        input_data = np.random.rand(2).astype(np.float32)
-        session = tf2onnx.backend.prepare(onnx_model)
-        prediction = session.run(input_data)
-        print(f"ONNX model prediction: {prediction}")
-    elif command.startswith("env_step"):
-        # Simulate a step in the environment
-        action = np.random.randint(2)
-        observation, reward, done, _ = env.step(action)
-        print(f"Environment step - Observation: {observation}, Reward: {reward}, Done: {done}")
+@bot.command()
+async def define(ctx, acronym):
+    if acronym.upper() in acronym_dictionary:
+        meaning = acronym_dictionary[acronym.upper()]
+        await ctx.send(f"The acronym '{acronym.upper()}' stands for: {meaning}")
     else:
-        print("Invalid command. Please try again.")
+        await ctx.send(f"Sorry, I don't have a definition for the acronym '{acronym.upper()}'.")
 
-# Initialize models and environment
-scikit_model = ScikitLearnModel()
-tf_model = None
-onnx_model = None
-env = SimpleEnvironment()
-
-# Example Usage
-if __name__ == "__main__":
-    process_command("train_sklearn")
-    process_command("train_tf")
-    process_command("convert_to_onnx")
-    process_command("predict_sklearn")
-    process_command("predict_onnx")
-    process_command("env_step"
-
-
-# Define a scikit-learn model
-class ScikitLearnModel
-    def __init__(self):
-        self.model = LogisticRegression()
-
-    def train(self, X, y):
-        self.model.fit(X, y)
-
-    def predict(self, X):
-        return self.model.predict(X)
-
-# Define a simple neural network using TensorFlow
-class SimpleNeuralNetwork(tf.keras.Model):
-    def __init__(self):
-        super(SimpleNeuralNetwork, self).__init__()
-        self.dense = tf.keras.layers.Dense(1, activation='sigmoid')
-
-    def call(self, inputs):
-        return self.dense(inputs)
-
-# Convert TensorFlow model to ONNX format
-def convert_to_onnx(model):
-    dummy_input = tf.constant(np.zeros((1, 2), dtype=np.float32))  # Adjust input shape as needed
-    onnx_model = tf2onnx.convert.from_keras(model, dummy_input)
-    return onnx_model
-
-# Define a simple environment using OpenAI Gym
-class SimpleEnvironment(gym.Env):
-    def __init__(self):
-        super(SimpleEnvironment, self).__init__()
-        self.action_space = gym.spaces.Discrete(2)
-        self.observation_space = gym.spaces.Box(low=0, high=1, shape=(2,), dtype=np.float32)
-
-    def reset(self):
-        return np.random.rand(2)
-
-    def step(self, action):
-        return np.random.rand(2), 0, False, {}
-
-# Self-learning Algorithm
-class SelfLearningAlgorithm:
-    def __init__(self):
-        self.model = SGDClassifier()
-
-    def learn(self, X, y):
-        self.model.partial_fit(X, y, classes=[0, 1])
-
-# Main command processing function
-def process_command(command):
-    global scikit_model, tf_model, onnx_model, self_learner
-    if command.startswith("train_sklearn"):
-        # Parse and preprocess training data
-        X = np.random.rand(100, 2)
-        y = np.random.randint(2, size=100)
-        scikit_model.train(X, y)
-        print("Scikit-learn model trained.")
-    elif command.startswith("train_tf"):
-        # Define and train a TensorFlow model
-        X = np.random.rand(100, 2)
-        y = np.random.randint(2, size=100)
-        tf_model = SimpleNeuralNetwork()
-        tf_model.compile(optimizer='adam', loss='binary_crossentropy')
-        tf_model.fit(X, y, epochs=5, verbose=0)
-        print("TensorFlow model trained.")
-    elif command.startswith("convert_to_onnx"):
-        # Convert TensorFlow model to ONNX
-        onnx_model = convert_to_onnx(tf_model)
-        print("TensorFlow model converted to ONNX.")
-    elif command.startswith("predict_sklearn"):
-        # Generate a random input and make a prediction using scikit-learn model
-        input_data = np.random.rand(2).reshape(1, -1)
-        prediction = scikit_model.predict(input_data)
-        print(f"Scikit-learn model prediction: {prediction}")
-    elif command.startswith("predict_onnx"):
-        # Simulate inference using ONNX model
-        input_data = np.random.rand(2).astype(np.float32)
-        session = tf2onnx.backend.prepare(onnx_model)
-        prediction = session.run(input_data)
-        print(f"ONNX model prediction: {prediction}")
-    elif command.startswith("env_step"):
-        # Simulate a step in the environment
-        action = np.random.randint(2)
-        observation, reward, done, _ = env.step(action)
-        print(f"Environment step - Observation: {observation}, Reward: {reward}, Done: {done}")
-    elif command.startswith("self_learn"):
-        # Simulate self-learning
-        X = np.random.rand(10, 2)
-        y = np.random.randint(2, size=10)
-        self_learner.learn(X, y)
-        print("Self-learning complete.")
+@bot.command()
+async def analyze_sentiment(ctx, *, text):
+    sentiment = model.predict([text])[0]
+    if sentiment == 1:
+        await ctx.send("Positive sentiment detected!")
     else:
-        print("Invalid command. Please try again.")
+        await ctx.send("Negative sentiment detected!")
 
-# Initialize models, environment, and self-learner
-scikit_model = ScikitLearnModel()
-tf_model = None
-onnx_model = None
-env = SimpleEnvironment()
-self_learner = SelfLearningAlgorithm()
-
-
-bot.run('')  # Replace with your bot token
-
-
-
+bot.run("")
